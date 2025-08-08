d4e97b2365a122207549a5d9bb9af2e3b1697303
chetan-914 <chetanjunja914@gmail.com>
2025-08-08 13:49:28 +0530

    add public list and detail endpoints for product

---
+++ products/serializers.py
@@ -0,0 +1,23 @@
+from rest_framework import serializers
+from .models import Category, Product
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('name', 'slug')
+
+class ProductSerializer(serializers.ModelSerializer):
+    category = CategorySerializer(read_only=True)
+
+    class Meta:
+        model = Product
+        fields  = (
+            'id',
+            'name',
+            'description',
+            'price',
+            'stock',
+            'category',
+            'image_url',
+            'is_available',
+        )
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListView(generics.ListAPIView):
+    queryset = Product.objects.filter(is_available=True)
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
+
+class ProductDetailView(generics.RetrieveAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    permission_classes = [permissions.AllowAny]
+    lookup_field = 'pk'
